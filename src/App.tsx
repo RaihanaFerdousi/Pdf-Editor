d7e2cda8a6a445c921903439595dc42faf06e8ec
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2026-03-28 20:30:10 +0600

    added funtionalitiess for rectangle buttion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import ToolBar from "./Components/ToolBar";
+import Canvas from "./Components/Canvas";
 
 export default function App() {
   return (
     <div style={{ display: "flex" }}>
-      <ToolBar/>
+      <Canvas/>
     </div>
   );
 }
--- src/Components/ToolBar.tsx
+++ src/Components/ToolBar.tsx
@@ -1,18 +1,45 @@
-import './ToolBar.css';
+import './Style.css';
 import { FaRegCircle } from "react-icons/fa";
 import { FaRegSquare } from "react-icons/fa";
 import { RiTriangleLine } from "react-icons/ri";
 import { IoPencil } from "react-icons/io5";
 import { GiStraightPipe } from "react-icons/gi";
+import { Canvas as FabricsCanvas, Rect } from 'fabric';
+import { LuRectangleHorizontal } from 'react-icons/lu';
 
+type ToolBarProps = {
+  canvas: FabricsCanvas | null;
+};
 
-export default function ToolBar() {
+export default function ToolBar({ canvas }: ToolBarProps) {
+  const addRectangle = () => {
+    if (!canvas) return;
+
+    const rect = new Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 150,
+      height: 80
+    });
+
+    canvas.add(rect);
+    canvas.renderAll();
+  }
+
   return (
     <div className="layout-container">
       <aside className="sidebar-toolbar">
-        <button 
+        <button
+          onClick={addCircle}
           className="toolbar-button">
-          <FaRegCircle/>
+          <FaRegCircle />
+        </button>
+
+        <button
+          onClick={addRectangle}
+          className="toolbar-button">
+          <LuRectangleHorizontal />
         </button>
 
         <button className="toolbar-button">
